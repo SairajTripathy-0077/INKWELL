9dc952ab42a824ac04aa8ebe7b54d876d5644952
Sairaj Tripathy <sairajtripathy4@gmail.com>
2026-05-30 19:50:09 +0530

    added authController

---
+++ src/controllers/authController.js
@@ -0,0 +1,54 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+};
+
+exports.registerUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const UserExists = await User.findOne({ username });
+        if (userExists) {
+            return res.status(400).json({
+                success: false,
+                message: 'Username is already taken'
+            })
+        }
+        const user = await User.create({ username, password });
+
+        res.status(201).json({
+            success: true;
+            token: generateToken(user._id)
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await User.findOne({ username }).select('+password');
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid credentials'
+            })
+        }
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ success: false, message: 'Invalid credentials' });
+        }
+        res.status(200).json({
+            success: true,
+            token: generateToken(user._id)
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
